35498aca4a083b53485f114c46c0c2a3c0173d42
wangyaoting <wangyaoting@autodatas.net>
2025-03-10 11:18:43 +0800

    fix: readme

--- vite.config.ts
+++ vite.config.ts
@@ -15,10 +15,10 @@ export default defineConfig({
       ],
     }),
   ],
-  // server: {
-  //   host: "0.0.0.0",
-  //   port: 3000,
-  // },
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
   build: {
     // target: "esnext",
     outDir: "dist",
