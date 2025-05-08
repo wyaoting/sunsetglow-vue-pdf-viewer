36dd99f6554b9e96fea14da71ff3ee4fa7f5f098
wangyaoting <wangyaoting@autodatas.net>
2025-05-08 14:52:48 +0800

    fix: configOption.fileName bug and version 0.3.1

--- packages/components/download.vue
+++ packages/components/download.vue
@@ -5,11 +5,19 @@
 </template>
 <script lang="ts" setup>
 import { inject } from "vue";
+import { configOption } from "../config";
 import { fetchFileResultDownload } from "../utils/index";
 import { DownloadOutlined } from "@ant-design/icons-vue";
 const pdfFileUrl = inject("pdfFileUrl");
+function ensurePdfExtension(filename: string) {
+  return !filename.endsWith(".pdf") ? filename + ".pdf" : filename;
+}
 const handleDownload = () => {
-  pdfFileUrl && fetchFileResultDownload(pdfFileUrl as string);
+  pdfFileUrl &&
+    fetchFileResultDownload(
+      pdfFileUrl as string,
+      ensurePdfExtension(configOption?.value?.fileName || "preview.pdf")
+    );
 };
 </script>
 
